b001a0ceab68f19b75cdf5877dff7c5af404bfa4
Sara <sarawwemadrid@gmail.com>
2022-07-20 16:26:32 +0200

    nav

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
 
 @Component({
   selector: 'app-background',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./background.component.scss']
 })
 export class BackgroundComponent implements OnInit {
+  screen: number = 1;
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => { //detectamos cambio de url
+      if (event instanceof NavigationEnd) {
+        if (event.url.includes('list')) {
+          this.screen = 2;    // en la pantalla intermedia de la lista de una familia, mostramos 2 capas
+        } else if (event.url.includes('item')) {
+          this.screen = 1;  // en la pantalla de un item, mostramos 1 capa
+        } else {
+          this.screen = 3; // en la pantalla principal, mostramos las 3 capas
+        }
+         
+      }
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  getBlur(px: number) { //adapta los píxeles al formato de blur
+    return 'blur('+px+'px)';
   }
 
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  isHome: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router ) {
+    this.router.events.subscribe((event: Event) => { //detectamos cambio en la url de navegación
+      if (event instanceof NavigationEnd) {
+        console.log(event.url)
+        if (event.url === '/') {
+          this.isHome = true; 
+        } else {
+          this.isHome = false;
+        }
+      }
+    });
+   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  navigateHome() {
+    this.router.navigate(['']);
   }
 
 }
